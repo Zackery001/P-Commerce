c2c2af2e5dcd080b65049be393fe27a23c326023
Zackery001 <therealhacker210@gmail.com>
2023-05-21 22:05:46 +0600

    Added index file

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include 'element/header.php'; ?>
+<?php include 'core/Stock.php'; ?>
+
+<div class="container">
+    <div class="row">
+
+        <h2>Hello, from P-Commerce!</h2>
+
+    </div>
+</div>
+
+<?php include 'element/footer.php'; ?>
