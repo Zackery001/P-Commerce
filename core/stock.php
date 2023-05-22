db4c7d872a4d61baa63ab156826b4630f20b9edb
Zackery001 <therealhacker210@gmail.com>
2023-05-22 10:51:27 +0600

    Added function to insert category in stock class

--- core/stock.php
+++ core/stock.php
@@ -4,6 +4,11 @@ include_once 'core/database.php';
 
 class Stock extends Database{
 
+    public function addCategory($category){
+
+        $sql = "INSERT INTO categories(category) VALUES ('$category')";
+        $this->exec($sql);
+    }
     
 }
 
