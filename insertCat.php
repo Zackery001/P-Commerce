e6192c5a2be54710254aa3e37085400172f1aa87
Zackery001 <therealhacker210@gmail.com>
2023-05-22 10:53:38 +0600

    Added insert category page

---
+++ insertCat.php
@@ -0,0 +1,37 @@
+<?php include "element/header.php";  ?>
+<?php include 'element/navbar.php'; ?>
+<?php include "core/Stock.php";  ?>
+
+<div class="container">
+	<div class="row">
+
+		<h1>This the insert category page where new data(categories) are inserted</h1>
+
+		<?php 
+		
+		if(isset($_POST['insert'])){
+
+			$stock = new Stock;
+            $stock->addCategory($_POST['category']);
+            echo "<p class='alert alert-success'>
+                Your category has been successfully inserted!</p>";
+				
+		}
+		
+		?>
+
+    	<div class="container mt-3">
+			<div class="border border-primary row p-2 col-8 offset-2 card">
+				
+				<form action="" method="POST">					
+					<p>Insert a category :</p>
+					<textarea name = "category" id = "textarea" class = "border border-primary mt-2 form-control" rows="1" required></textarea>
+                	<input type = "submit" name = "insert" value = "Insert" class = "mt-2 mb-1 btn btn-sm btn-success">
+				</form>
+
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include "element/footer.php"; ?>
