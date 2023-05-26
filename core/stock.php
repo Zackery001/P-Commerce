6df6b413dab07c1e88a39783f1e02193c543e138
Zackery001 <therealhacker210@gmail.com>
2023-05-26 19:11:24 +0600

    Added Queries to Insert Item.

--- core/stock.php
+++ core/stock.php
@@ -4,10 +4,31 @@ include_once 'core/database.php';
 
 class Stock extends Database{
 
-    public function addCategory($category){
-
+    //Category
+    
+    public function getCategory()
+    {
+        $sql = 'SELECT * FROM categories';
+        return $this->fetch($sql);
+    }
+    public function addCategory($category)
+    {
         $sql = "INSERT INTO categories(category) VALUES ('$category')";
         $this->exec($sql);
+    }
+
+    //Items
+    public function addItem($category_id, $details, $cost_price, $selling_price)
+    {
+        $sql = "INSERT INTO items(category_id, details, cost_price, selling_price) VALUES(
+            '$category_id', '$details', '$cost_price', '$selling_price') ";
+        $this->exec($sql);
+    }
+
+    public function duplicateItem($details)
+    {
+        $sql = "SELECT * FROM items WHERE details = '$details'";
+        return $this->fetch($sql);
     }
     
 }
